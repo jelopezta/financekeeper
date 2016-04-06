4cc64fdb6bd4e7d4ad0c2c802a16ec9328536ebf
jelopezta <jelopezta@gmail.com>
2016-04-05 22:29:45 -0500

    Menu Drawer item changes
    Menu Drawer item changes. added add acounts, remove acounts, settings.
    Updated id values in file and activity. Externalize label strings

--- app/src/main/java/com/divdev/financekeeper/FinanceKeeperMainActivity.java
+++ app/src/main/java/com/divdev/financekeeper/FinanceKeeperMainActivity.java
@@ -80,13 +80,11 @@ public class FinanceKeeperMainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.add_acount_menu_item) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.remove_accounts_menu_item) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.settings_menu_item) {
 
         } else if (id == R.id.nav_share) {
 
