50a68b427a8e4b4d09f30fa3e9e9ddcec141c662
jelopezta <jelopezta@gmail.com>
2016-04-09 09:19:57 -0500

    package refactor
    package refactor. moving activities to ui dedicated package

--- app/src/main/java/com/divdev/financekeeper/ui/FinanceKeeperMainActivity.java
+++ app/src/main/java/com/divdev/financekeeper/ui/FinanceKeeperMainActivity.java
@@ -1,4 +1,4 @@
-package com.divdev.financekeeper;
+package com.divdev.financekeeper.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.divdev.financekeeper.R;
 
 public class FinanceKeeperMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
