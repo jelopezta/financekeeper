9d5ee7f55220107577af7def462b5ac39cca9504
jelopezta <jelopezta+divdev@gmail.com>
2016-05-09 12:10:33 -0500

    Activity to support app's operations on finance nodes
    Activity to support app's operations on finance nodes

--- app/src/main/java/com/divdev/financekeeper/presentation/activities/FinanceKeeperMainActivity.java
+++ app/src/main/java/com/divdev/financekeeper/presentation/activities/FinanceKeeperMainActivity.java
@@ -1,11 +1,9 @@
 package com.divdev.financekeeper.presentation.activities;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,7 +13,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.divdev.financekeeper.FinanceKeeperApplication;
